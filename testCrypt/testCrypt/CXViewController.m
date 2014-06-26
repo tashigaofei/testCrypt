a6f10a2ff7e6e8315cf8a6227c0eb91d6e740a8f
tashigaofei <tashigaofei@gmail.com>
2014-06-26 15:41:52 +0800

    commit

---
+++ testCrypt/testCrypt/CXViewController.m
@@ -0,0 +1,29 @@
+//
+//  CXViewController.m
+//  testCrypt
+//
+//  Created by tashigaofei on 14-6-26.
+//  Copyright (c) 2014年 AliPay. All rights reserved.
+//
+
+#import "CXViewController.h"
+
+@interface CXViewController ()
+
+@end
+
+@implementation CXViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
